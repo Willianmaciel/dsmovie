6bcb508ce7ae043d7a50be72b804c60d83f02000
Willian Maciel <willianlpa7x@gmail.com>
2022-01-16 19:00:34 -0400

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,20 +4,19 @@ import './style.css';
 function Navbar(){
 
     return(
-        <header>
-          <nav className="container">
-            <div className='dsmovie-nav-content'>
+      <header>
+      <nav className="container">
+          <div className="dsmovie-nav-content">
               <h1>DSMovie</h1>
-              <a href="https://github.com/Willianmaciel">
-              <div className="dsmovie-contact-container">
-                <GithubIcon />
-                <p className="dsmovie-contact-link">Willian</p>
-              </div>
-              
+              <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                  <div className="dsmovie-contact-container">
+                      <GithubIcon />
+                      <p className="dsmovie-contact-link">/Willian</p>
+                  </div>
               </a>
-            </div>
-            </nav>
-      </header>
+          </div>
+      </nav>
+  </header>
     );
 
 }
