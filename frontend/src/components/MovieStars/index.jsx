928bfdef75c957b551a9d33f6f49786ddf0cabde
Willian Maciel <willianlpa7x@gmail.com>
2022-01-16 22:22:35 -0400

    Navigation buttons

--- frontend/src/components/MovieStars/index.jsx
+++ frontend/src/components/MovieStars/index.jsx
@@ -1,7 +1,7 @@
-import { ReactComponet as StarFull} from 'assets/img/star-full.svg';
-import { ReactComponet as StarHalf} from 'assets/img/star-half.svg';
-import { ReactComponet as StarEmpty} from 'assets/img/star-empty.svg';
-import './>styles.css';
+import { ReactComponent as StarFull} from 'assets/img/star-full.svg';
+import { ReactComponent as StarHalf} from 'assets/img/star-half.svg';
+import { ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
+import './styles.css'
 
 function MovieStars() {
     return (
